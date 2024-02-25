52d5920a9b8d00d33c2fd43c803a402c3b1ce7db
joshiaLee <93809073+joshiaLee@users.noreply.github.com>
2024-02-25 20:50:25 +0900

    [Bronze II] Title: 거리의 합, Time: 936 ms, Memory: 110692 KB -BaekjoonHub

--- 백준/Bronze/2399. 거리의 합/거리의 합.py
+++ 백준/Bronze/2399. 거리의 합/거리의 합.py
@@ -1,3 +1,3 @@
 n = int(input())
 l = [int(i) for i in input().split(" ")]
-print(sum(abs(l[i] - l[j]) for i in range(n) for j in range(n)))
+print(sum(abs(l[i] - l[j]) for i in range(n) for j in range(i+1, n)) * 2)
