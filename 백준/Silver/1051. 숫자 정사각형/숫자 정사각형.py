c26e2737f74af1b522e2539851ebee91c34ed991
joshiaLee <93809073+joshiaLee@users.noreply.github.com>
2024-02-27 09:52:04 +0900

    [Silver III] Title: 숫자 정사각형, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -7,9 +7,15 @@ min_val = 1
 max_val = min(n,m)
 
 for side in range(2, max_val+1):
+    flag = 0
     for i in range(n-(side-1)):
+        if flag:
+            break
         for j in range(m-(side-1)):
+            if flag:
+                break
             if l[i][j] == l[i+(side-1)][j] == l[i][j+(side-1)] == l[i+(side-1)][j+(side-1)]:
                 min_val = side
+                flag = 1
 
 print(min_val*min_val)    
