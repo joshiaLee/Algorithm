952c2ae3ec2d208a0b4050df719a348fb8953f99
joshiaLee <93809073+joshiaLee@users.noreply.github.com>
2024-03-10 09:25:04 +0900

    [Bronze IV] Title: 평균 점수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.py
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -1,3 +1,2 @@
 import sys
-score = sys.stdin.read().strip() 
-print(sum(int(i) if int(i) > 40 else 40 for i in score.split("\n")) // 5)
+print(sum(int(i) if int(i) > 40 else 40 for i in sys.stdin.read().strip().split("\n")) // 5)
