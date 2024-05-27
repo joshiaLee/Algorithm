72db5633f1c36275698edcfaabaf4d56ff4e11a5
joshiaLee <93809073+joshiaLee@users.noreply.github.com>
2024-05-27 10:28:50 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t,s;
+    cin >> t >> s;
+
+    if(s == 0 && (t >= 12 && t <= 16)){
+        cout << "320";
+    }
+
+    else{
+        cout << "280";
+    }
+
+}
